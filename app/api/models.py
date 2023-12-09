3a70ebcbce1fa73402d3ffc4c69df75bde9e6bd3
AfroStorm <maloa.elias@yahoo.com>
2023-12-09 04:08:42 +0100

    Modifying models

--- app/api/models.py
+++ app/api/models.py
@@ -36,7 +36,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     """A custom user model."""
 
     email = models.EmailField(max_length=255, unique=True)
-    username = models.CharField(max_length=50)
 
     objects = CustomUserManager()
 
@@ -44,15 +43,22 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     def __str__(self) -> str:
         return f'Email: {self.email}; Username: {self.username}'
 
 
-class Tag(models.Model):
+class Priority(models.Model):
     """A tag that declares the priority of a task."""
 
+    caption = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f'{self.caption}'
+
+
+class Status(models.Model):
+    """A tag that shows the current status of a task."""
     caption = models.CharField(max_length=30)
 
     def __str__(self) -> str:
@@ -68,6 +74,50 @@ class Category(models.Model):
         return f'{self.name}'
 
 
+class Position(models.Model):
+    """Describes the position/role of a user/team member."""
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+
+class TaskGroup(models.Model):
+
+    """Overview of all the users who work on a task."""
+
+    def calculate_members(self):
+        """returns the current number of team members for a task."""
+        pass
+
+    def __str__(self) -> str:
+        return f'Task ID: {self.task.id}, Group ID: {self.id}'
+
+
+class UserProfile(models.Model):
+    """A profile for each user."""
+    owner = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.DO_NOTHING,
+        related_name='position_userprofile_field'
+    )
+    task_group = models.ForeignKey(
+        TaskGroup,
+        on_delete=models.CASCADE,
+        related_name='task_group_userprofile_field'
+    )
+
+
 class Task(models.Model):
     """A task with different status options."""
 
@@ -78,28 +128,23 @@ class Task(models.Model):
     category = models.OneToOneField(
         Category,
         on_delete=models.DO_NOTHING,
-        related_name='the_task'
+        related_name='category_task_field'
     )
     priority = models.OneToOneField(
-        Tag,
+        Priority,
         on_delete=models.DO_NOTHING,
-        related_name='task'
+        related_name='priority_task_field'
     )
-
-    def __str__(self) -> str:
-        return f'{self.title}, ID: {self.id}'
-
-
-class TaskGroup(models.Model):
-    """Overview of all the users who work on a task."""
-
-    participants = models.ManyToManyField(CustomUser)
-    task = models.OneToOneField(
-        Task,
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.DO_NOTHING,
+        related_name='status_task_field'
+    )
+    task_group = models.OneToOneField(
+        TaskGroup,
         on_delete=models.CASCADE,
-        related_name='task_group'
-
+        related_name='task_group_task_field'
     )
 
     def __str__(self) -> str:
-        return f'Task ID: {self.task.id}, Group ID: {self.id}'
+        return f'Title: {self.title} - ID: {self.id} - Status: {self.status}'
--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dhtl6BeYrKhctg1MZvNbwptinWegF0FZ84ZjyNqHzSGk8BgTU0'
+SECRET_KEY = 'rTuwCat7iSfb8mxRyJLyj7ToVx9jZwEZtkFPOqKukjRhTnNHef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
