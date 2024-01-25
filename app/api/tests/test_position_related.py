35e22e4fc9838597a96b646b4d142395d72013ca
AfroStorm <maloa.elias@yahoo.com>
2024-01-25 19:18:47 +0100

    Restructure category, position, priority tests

--- app/api/tests/test_position_related.py
+++ app/api/tests/test_position_related.py
@@ -13,232 +13,241 @@ class TestPositionModel(APITestCase):
     """Tests that are related to the Position model."""
 
     def setUp(self) -> None:
+        '''The creation of the following instances are necessary to test the
+        position view nd eventual serializer, signal handler etc.'''
+
+        # Deactivate signal handlers for more control over setUp instances
         post_save.disconnect(signals.create_or_update_profile, sender=User)
         post_save.disconnect(signals.create_task_group, sender=models.Task)
 
-        # Creating user instance 1
-        self.user = User.objects.create(
+        # Creating user instances
+        self.regular_user = User.objects.create(
             email='peterpahn@gmail.com',
             password='blabla123.'
         )
-
-        # Creating user instance 2
-        self.user2 = User.objects.create(
+        self.admin_user = User.objects.create(
             email='tinaturner@gmail.com',
-            password='blabla123.'
+            password='blabla123.',
+            is_staff=True
         )
 
-        # Category instance
+        # Creating category instances
         self.human_resource_category = models.Category.objects.create(
             name='Human Resource Management',
-            description='Human Resource Management task category involves'
-            'overseeing recruitment, employee development, performance'
-            'evaluation, and maintaining a positive workplace culture to'
-            'optimize the organizations human capital.'
+            description='''Human Resource Management task category involves
+            overseeing recruitment, employee development, performance
+            evaluation, and maintaining a positive workplace culture to
+            optimize the organizations human capital.'''
         )
-
-        self.marketing_category = models.Category.objects.create(
-            name='Marketing',
-            description='Marketing category involves creating, communicating, delivering, '
-            'and exchanging offerings that have value for customers. It includes '
-            'activities such as market research, advertising, and brand management.'
-
-        )
-
         self.financial_category = models.Category.objects.create(
             name='Financial Analysis',
-            description='Financial Analysis category involves assessing the '
-            'financial health of the organization by analyzing '
-            'financial statements, budgeting, forecasting, and '
-            'providing insights for strategic decision-making.'
-
+            description='''Financial Analysis category involves assessing
+            the financial health of the organization by analyzing 
+            financial statements, budgeting, forecasting, and 
+            providing insights for strategic decision-making.'''
         )
 
-        self.it_category = models.Category.objects.create(
-            name='Information Technology',
-            description='Information Technology category involves managing and '
-            'maintaining the organization\'s technology infrastructure, '
-            'networks, and systems. It includes tasks such as software '
-            'development, IT support, and cybersecurity.'
-
-        )
-
-        # Position instance
+        # Creating position instances
         self.human_resource_position = models.Position.objects.create(
             title='Human Resource Specialist',
-            description='A Human Resource Specialist focuses on recruitment,'
-            'employee relations, benefits administration, and workforce'
-            'planning, ensuring effective management of human resources'
-            'within an organization.',
+            description='''A Human Resource Specialist focuses on
+            recruitment, employee relations, benefits administration, and
+            workforce planning, ensuring effective management of human
+            resources within an organization.''',
             is_task_manager=False,
             related_category=self.human_resource_category
         )
-
-        self.marketing_position = models.Position.objects.create(
-            title='Marketing Coordinator',
-            description='A Marketing Coordinator assists in the implementation of marketing '
-            'strategies and campaigns. They may handle tasks like coordinating '
-            'events, managing social media, and supporting brand promotion efforts.',
-            is_task_manager=False,
-            related_category=self.marketing_category
-        )
-
         self.financial_position = models.Position.objects.create(
             title='Financial Analyst',
-            description='A Financial Analyst specializes in financial planning,'
-            'budgeting, and analysis. They assess financial data, prepare'
-            'reports, and provide recommendations to support strategic'
-            'financial decision-making within the organization.',
+            description='''A Financial Analyst specializes in financial
+            planning, budgeting, and analysis. They assess financial data,
+            prepare reports, and provide recommendations to support strategic
+            financial decision-making within the organization.''',
             is_task_manager=False,
             related_category=self.financial_category
         )
 
-        # Creating userprofile instance 1
-        self.userprofile = models.UserProfile.objects.create(
-            owner=self.user,
+        # Creating userprofile instances
+        self.regular_userprofile = models.UserProfile.objects.create(
+            owner=self.regular_user,
             first_name='Peter',
             last_name='Pahn',
             phone_number=int('0163557799'),
-            email=self.user.email,
+            email=self.regular_user.email,
             position=self.human_resource_position
         )
-
-        # Creating userprofile instance 2
-        self.userprofile2 = models.UserProfile.objects.create(
-            owner=self.user2,
+        self.admin_userprofile = models.UserProfile.objects.create(
+            owner=self.admin_user,
             first_name='Tina',
             last_name='Turner',
             phone_number=int('0176559934'),
-            email=self.user2.email,
-            position=self.financial_position
+            email=self.admin_user.email,
+            position=self.human_resource_position
         )
 
     # List view
     def test_authenticated_user_can_access_list(self):
         """Tests if the list view action allows authenticated users."""
 
-        self.client.force_authenticate(user=self.user)
+        # Authenticated user
+        self.client.force_authenticate(user=self.regular_user)
+
         url = reverse('position-list')
         response = self.client.get(url)
 
+        # Check if access is granted
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_unauthenticated_user_cant_access_list(self):
         """Tests if the list view action disallows unauthenticated users."""
 
+        # Unauthenticated user
+
         url = reverse('position-list')
         response = self.client.get(url)
 
+        # Check if access is denied
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # Retrieve view
     def test_authenticated_user_can_access_retrieve(self):
         """Tests if the retrieve view action allows authenticated users."""
 
-        self.client.force_authenticate(user=self.user)
-        url = reverse('position-detail',
-                      args=[self.human_resource_position.id])
+        # Authenticated user
+        self.client.force_authenticate(user=self.regular_user)
+
+        url = reverse(
+            'position-detail',
+            args=[self.human_resource_position.id]
+        )
         response = self.client.get(url)
 
+        # Check if access is granted
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_unauthenticated_user_cant_access_retrieve(self):
         """Tests if the retrieve view action disallows unauthenticated users.
         """
 
-        url = reverse('position-detail',
-                      args=[self.human_resource_position.id])
+        # Unauthenticated user
+
+        url = reverse(
+            'position-detail',
+            args=[self.human_resource_position.id]
+        )
         response = self.client.get(url)
 
+        # Check if access is denied
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # Create view
     def test_staff_can_access_create(self):
         """Tests if the create view action allows staff users."""
 
-        self.user.is_staff = True
-        self.client.force_authenticate(user=self.user)
+        # Staff user
+        self.client.force_authenticate(user=self.admin_user)
 
         url = reverse('position-list')
+        it_category = models.Category.objects.create(
+            name='Information Technology',
+            description='''Information Technology category involves managing
+            and maintaining the organization\s technology infrastructure,
+            networks, and systems. It includes tasks such as software 
+            development, IT support, and cybersecurity.'''
+        )
         data = {
             'title': 'IT Specialist',
-            'description': 'An IT Specialist is responsible for maintaining and'
-            'troubleshooting the organization\'s computer systems,'
-            'networks, and software. They provide technical support,'
-            'implement security measures, and ensure the smooth operation'
-            'of IT resources.',
+            'description': '''An IT Specialist is responsible for
+                maintaining and troubleshooting the organization\'s computer
+                systems, networks, and software. They provide technical
+                support, implement security measures, and ensure the smooth
+                operation of IT resources.''',
             'is_task_manager': False,
-            'related_category': self.it_category.name,
-
+            'related_category': it_category.name,
         }
-
         response = self.client.post(url, data, format='json')
 
-        # Check if the task was created
+        # Check if access is granted
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_non_staff_cant_access_create(self):
         """Tests if the create view action diallows non staff users."""
 
-        self.client.force_authenticate(user=self.user)
+        # Non-staff user
+        self.client.force_authenticate(user=self.regular_user)
 
         url = reverse('position-list')
+        it_category = models.Category.objects.create(
+            name='Information Technology',
+            description='''Information Technology category involves managing
+            and maintaining the organization\s technology infrastructure,
+            networks, and systems. It includes tasks such as software 
+            development, IT support, and cybersecurity.'''
+        )
         data = {
             'title': 'IT Specialist',
-            'description': 'An IT Specialist is responsible for maintaining and'
-            'troubleshooting the organization\'s computer systems,'
-            'networks, and software. They provide technical support,'
-            'implement security measures, and ensure the smooth operation'
-            'of IT resources.',
+            'description': '''An IT Specialist is responsible for
+                maintaining and troubleshooting the organization\'s computer
+                systems, networks, and software. They provide technical
+                support, implement security measures, and ensure the smooth
+                operation of IT resources.''',
             'is_task_manager': False,
-            'related_category': self.it_category.name
+            'related_category': it_category.name,
         }
-
         response = self.client.post(url, data, format='json')
 
-        # Check if the permission is denied for non staff users
+        # Check if access is denied
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # Update view
     def test_staff_can_access_update(self):
         """Tests if the update view action allows staff users."""
 
-        self.user2.is_staff = True
-        self.client.force_authenticate(user=self.user2)
+        # Staff user
+        self.client.force_authenticate(user=self.admin_user)
 
-        url = reverse('position-detail', args=[self.financial_position.id])
+        url = reverse(
+            'position-detail',
+            args=[self.human_resource_position.id]
+        )
         data = {
             'title': 'Financial Analyst',
             'description':
-                'A Financial Analyst specializes in financial planning,'
-                'budgeting, and analysis. They assess financial data, prepare'
-                'reports, and provide recommendations to support strategic'
-                'financial decision-making within the organization.',
-                'is_task_manager': False,
-                'related_category': self.financial_category.name
+                '''A Financial Analyst specializes in financial planning,
+                budgeting, and analysis. They assess financial data, prepare
+                reports, and provide recommendations to support strategic
+                financial decision-making within the organization.''',
+            'is_task_manager': False,
+            'related_category': self.human_resource_category.name
         }
-
         response = self.client.put(url, data, format='json')
+
+        # Check if access is granted
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_non_staff_cant_access_update(self):
         """Tests if the update view action disallows non staff users."""
 
-        self.client.force_authenticate(user=self.user2)
+        # Non-staff user
+        self.client.force_authenticate(user=self.regular_user)
 
-        url = reverse('position-detail', args=[self.financial_position.id])
+        url = reverse(
+            'position-detail',
+            args=[self.human_resource_position.id]
+        )
         data = {
             'title': 'Financial Analyst',
             'description':
-                'A Financial Analyst specializes in financial planning,'
-                'budgeting, and analysis. They assess financial data, prepare'
-                'reports, and provide recommendations to support strategic'
-                'financial decision-making within the organization.',
-                'is_task_manager': False,
-                'related_category': self.financial_category.name
+                '''A Financial Analyst specializes in financial planning,
+                budgeting, and analysis. They assess financial data, prepare
+                reports, and provide recommendations to support strategic
+                financial decision-making within the organization.''',
+            'is_task_manager': False,
+            'related_category': self.human_resource_category.name
         }
-
         response = self.client.put(url, data, format='json')
+
+        # Check if access is denied
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # Partial update view
@@ -246,55 +255,73 @@ class TestPositionModel(APITestCase):
         """Tests if the partial update view action allows staff
         users."""
 
-        self.user2.is_staff = True
-        self.client.force_authenticate(user=self.user2)
+        # Staff user
+        self.client.force_authenticate(user=self.admin_user)
 
-        url = reverse('position-detail', args=[self.financial_position.id])
+        url = reverse(
+            'position-detail',
+            args=[self.human_resource_position.id]
+        )
         data = {
             'title': 'Financial Analyst',
             'is_task_manager': False,
-            'related_category': self.financial_category.name
+            'related_category': self.human_resource_category.name
         }
-
         response = self.client.patch(url, data, format='json')
+
+        # Check if access is granted
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_non_staff_cant_access_partial_update(self):
         """Tests if the partial update view action disallows non staff
         users."""
 
-        self.client.force_authenticate(user=self.user2)
+        # Non-staff user
+        self.client.force_authenticate(user=self.regular_user)
 
-        url = reverse('position-detail', args=[self.financial_position.id])
+        url = reverse(
+            'position-detail',
+            args=[self.human_resource_position.id]
+        )
         data = {
             'title': 'Financial Analyst',
             'is_task_manager': False,
-            'related_category': self.financial_category.name
+            'related_category': self.human_resource_category.name
         }
-
         response = self.client.patch(url, data, format='json')
+
+        # Check if access is denied
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # Destroy view
     def test_staff_can_access_destroy(self):
         """Tests if the destroy view action allows staff users."""
 
-        self.user2.is_staff = True
-        self.client.force_authenticate(user=self.user2)
+        # Staff user
+        self.client.force_authenticate(user=self.admin_user)
 
-        url = reverse('position-detail', args=[self.marketing_position.id])
-
+        url = reverse(
+            'position-detail',
+            args=[self.financial_position.id]
+        )
         response = self.client.delete(url)
+
+        # Check if access is granted
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_non_staff_cant_access_destroy(self):
         """Tests if the destroy view action disallows non staff users."""
 
-        self.client.force_authenticate(user=self.user2)
+        # Non-staff user
+        self.client.force_authenticate(user=self.regular_user)
 
-        url = reverse('position-detail', args=[self.marketing_position.id])
-
+        url = reverse(
+            'position-detail',
+            args=[self.financial_position.id]
+        )
         response = self.client.delete(url)
+
+        # Check if access is denied
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # Serializer
@@ -302,8 +329,9 @@ class TestPositionModel(APITestCase):
         """Tests if the position serializer is correctly setting certain
         fields to a slug field for all users."""
 
+        self.client.force_authenticate(user=self.regular_user)
+
         url = reverse('position-list')
-        self.client.force_authenticate(user=self.user)
         response = self.client.get(url, format='json')
         request = response.wsgi_request
 
